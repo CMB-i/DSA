70a93cdfb8ad925dd17d5aadf5088019a1072472
CMB-i <charvibayana@gmail.com>
2026-06-28 20:37:25 +0530

    18

---
+++ leet75/arrays/18_LargestAltitude.py
@@ -0,0 +1,6 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        ans = [0]*(len(gain)+1)
+        for i in range(1, len(gain)+1):
+              ans[i] = ans[i-1] + gain[i-1]
+        return max(ans)
